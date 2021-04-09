394490f43dbccd3435501b2173f3485bed0df94e
Suraj Verma <surajverma@rentalcars.com>
2021-04-09 22:31:02 +0100

    add uv index

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,18 +27,20 @@ const getDataByCityName = async (event) => {
   if (target.is("li")) {
     const cityName = target.data("city");
 
-    renderAllCards(cityName)
+    renderAllCards(cityName);
   }
 };
 
-const transformCurrentDayData = (data) => {
+const transformCurrentDayData = (data, name) => {
+  const current = data.current;
   return {
-    cityName: data.name,
-    temperature: data.main.temp,
-    humidity: data.main.humidity,
-    windSpeed: data.wind.speed,
-    date: moment.unix(data.dt).format("MM/DD/YYYY"),
-    iconURL: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+    cityName: name,
+    temperature: current.temp,
+    humidity: current.humidity,
+    windSpeed: current.wind_speed,
+    date: moment.unix(current.dt).format("MM/DD/YYYY"),
+    iconURL: `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`,
+    uvi: current.uvi,
   };
 };
 
@@ -65,10 +67,10 @@ const onSubmit = async (event) => {
 
   $("#city-input").val("");
 
-  renderAllCards(cityName)
+  renderAllCards(cityName);
 };
 
-const renderAllCards = (cityName) => {
+const renderAllCards = async (cityName) => {
   const currentDayUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${API_KEY}`;
 
   const currentDayResponse = await fetchData(currentDayUrl);
@@ -83,10 +85,13 @@ const renderAllCards = (cityName) => {
 
   cardsData.slice(1, 6).forEach(renderForecastCard);
 
-  const currentDayData = transformCurrentDayData(currentDayResponse);
+  const currentDayData = transformCurrentDayData(
+    forecastResponse,
+    currentDayResponse.name
+  );
 
   renderCurrentDayCard(currentDayData);
-}
+};
 
 const renderCitiesFromLocalStorage = () => {
   $("#searched-cities").empty();
@@ -122,7 +127,7 @@ const renderCurrentDayCard = (data) => {
       <div class="py-2">Temperature: ${data.temperature}&deg; F</div>
       <div class="py-2">Humidity: ${data.humidity}%</div>
       <div class="py-2">Wind Speed: ${data.windSpeed} MPH</div>
-      <div class="py-2">UV Index:</div>
+      <div class="py-2">UV Index: ${data.uvi}</div>
     </div>
   </div>`;
 
