6dc7388e5ed501e6b49bce0ce9487bb19af92d2d
Suraj Verma <surajverma@rentalcars.com>
2021-04-09 19:20:21 +0100

    save to local storage on submit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,22 @@
+const getFromLocalStorage = () => {
+  const localStorageData = JSON.parse(localStorage.getItem("cities"));
+
+  if (localStorageData === null) {
+    return [];
+  } else {
+    return localStorageData;
+  }
+};
+
 const onSubmit = (event) => {
   event.preventDefault();
 
   const cityName = $("#city-input").val();
-  console.log(cityName);
+  const cities = getFromLocalStorage();
+
+  cities.push(cityName);
+
+  localStorage.setItem("cities", JSON.stringify(cities));
 };
 
 $("#search-by-city-form").on("submit", onSubmit);
